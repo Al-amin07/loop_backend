76b60f4123e0a6c7bb4581042da2f6a3c4574e41
Al-amin07 <alsujon2001@gmail.com>
2025-01-10 11:27:08 +0600

    Initial commit

--- dist/app/modules/work/work.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.workRoute = void 0;
-const express_1 = require("express");
-const work_controller_1 = require("./work.controller");
-const route = (0, express_1.Router)();
-route.post('/', work_controller_1.workControllers.createWork);
-route.get('/', work_controller_1.workControllers.getAllWork);
-exports.workRoute = route;
